ab5679fff1a4f581a6d6a9d0a42e8eb79b346d83
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-07 22:57:34 +0100

    commit 11: task 10(It does not work yet)

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a list has a cycle.
+ * @list: pointer to head of list.
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp_list;
+
+	temp_list = list;
+	if (temp_list == NULL)
+		return (0);
+	return (1);
+}
