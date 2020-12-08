b0b5fc6879c80cbf2010b387aa0afbbb7183f57b
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-08 22:54:09 +0100

    commit 16: Task 9 fixing print format

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,5 @@ def print_last_digit(number):
     if number < 0:
         number *= -1
     last_digit = number % 10
-    print(last_digit)
+    print("{:d}".format(last_digit), end="")
     return (last_digit)
