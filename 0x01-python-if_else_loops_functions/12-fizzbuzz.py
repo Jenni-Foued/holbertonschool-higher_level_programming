b33bde8320dd6bd278b4f048ed4a8787846cec76
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-08 23:28:41 +0100

    commit 21: Task 12: Fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1,101):
-        if (i % 3 == 0) and (i % 5 == 0):
-            print("FizzBuzz", end="")
-        elif i % 3 == 0:
+    for i in range(1, 101):
+        if i % 3 == 0:
             print("Fizz", end="")
-        elif i % 5 == 0:
+        if i % 5 == 0:
             print("Buzz", end="")
-        else:
+        if i % 3 and i % 5:
             print("{:d}".format(i), end="")
-        if i < 100:
-            print(" ", end="")
+        print(end=" ")
