535c539f4fb0e44404b7f958014bc0e9e6d6151f
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-08 22:05:29 +0100

    commit 10: Task 6 fixing output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for first_digit in range(0, 8):
-    for second_digit in range (1 + first_digit, 10):
+    for second_digit in range(1 + first_digit, 10):
         print("{:d}{:d}".format(first_digit, second_digit), end=", ")
 print("{:d}{:d}".format(first_digit + 1, second_digit))
