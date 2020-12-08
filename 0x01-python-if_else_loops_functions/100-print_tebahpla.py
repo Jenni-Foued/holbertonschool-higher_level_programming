5c888476e42025d62f78c1eb6fd014b12da2c3b0
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-08 23:45:45 +0100

    commit 23: Advanced task 100: New approach

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-checker = 1
 for c in range(ord('z'), ord('a') - 1, -1):
-    print(chr(c) if checker else chr(c - 32), end="")
-    if checker == 1:
-        checker = 0
-    else:
-        checker = 1
+    print("{:c}".format((c - (32)) if c % 2 else c), end='')
