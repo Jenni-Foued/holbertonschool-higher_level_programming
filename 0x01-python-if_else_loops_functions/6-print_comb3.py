e59038f332480455d13f6ea4d2ee25458199c62a
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-08 22:02:04 +0100

    commit 9: Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for first_digit in range(0, 8):
+    for second_digit in range (0 + first_digit, 10):
+        print("{:d}{:d}".format(first_digit, second_digit), end=", ")
+print("{:d}{:d}".format(first_digit + 1, second_digit))
