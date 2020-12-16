6d855c4d2adaa51ab2b34ac7c71cbc4cce021fb5
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-16 08:44:57 +0100

    Task 4: Fix for idx 0

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@
 def new_in_list(my_list, idx, element):
     my_list_copy = []
     my_list_copy = my_list[:]
-    if idx > 0 and idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         my_list_copy[idx] = element
     return my_list_copy
