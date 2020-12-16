3f7c99440f875f7b20f3a950f37347098bcc01ed
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-16 09:57:51 +0100

    Task 10: better solution

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,8 +5,5 @@ def divisible_by_2(my_list=[]):
     result_list = []
     if my_list:
         for idx in range(0, len(my_list)):
-            if my_list[idx] % 2 == 0:
-                result_list.append(True)
-            else:
-                result_list.append(False)
+            result_list.append(False if my_list[idx] % 2 else True)
     return result_list
