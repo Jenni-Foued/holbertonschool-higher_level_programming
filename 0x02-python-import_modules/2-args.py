b54e475d7036de4df0f190983f7a736505bd8faa
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-16 20:58:48 +0100

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+import sys
+if len(sys.argv) == 1:
+    print("0 arguments.")
+else:
+    print("{:d} argument:".format(len(sys.argv) - 1) if (len(sys.argv) == 2)
+          else "{:d} arguments:".format(len(sys.argv) - 1))
+    for idx in range(1, len(sys.argv)):
+        print("{:d}: {}".format(idx, sys.argv[idx]))
