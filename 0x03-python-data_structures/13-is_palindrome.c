a3e181ca036cf4a1db8f197b80d211912cd2d880
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-16 10:23:46 +0100

    Task 13: not done yet

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * 
+ * @head: pointer to the first node of the list.
+ * 
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL)
+		return (1);
+	return (0);
+}
