cf84d134c014446e7d7cc55a287220fbc91bc7fd
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-16 09:20:50 +0100

    Task 7: fix E128 continuation line under-indented for visual indent

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,6 +3,7 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     a, b = len(tuple_a), len(tuple_b)
-    result_tuple = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
-                 (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
-    return result_tuple
+    res_tuple = (
+        (tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
+        (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
+    return res_tuple
