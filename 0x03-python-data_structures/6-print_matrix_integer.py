5162c27176390399e45a7744d775176a682af4de
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-16 08:35:11 +0100

    Task 6: fix range

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range (len(matrix[i])):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
             print("{}".format(matrix[i][j]), end='')
             if j != (len(matrix) - 1):
                 print("", end=' ')
