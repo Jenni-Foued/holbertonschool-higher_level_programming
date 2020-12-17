3d4e2e0ccab31121af801dba5a6e10aba7c30acd
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-17 13:35:36 +0100

    Task 2: a shorter solution using set()

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,12 +3,6 @@
 
 def uniq_add(my_list=[]):
     res = 0
-    if my_list:
-        my_list.sort()
-        prev = my_list[0]
-        res = prev
-        for i in my_list:
-            if i != prev:
-                res += i
-                prev = i
+    for i in set(my_list):
+        res += i
     return res
