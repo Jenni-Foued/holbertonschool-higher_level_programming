eb5163a1d1153d6295d0ddb37f02a7f69d8e694b
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-04 13:11:11 +0100

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        if x > 0:
+            i = 0
+            for elm in my_list:
+                print("{}".format(elm), end="")
+                i += 1
+                if i == x:
+                    break
+            print("")
+    except IndexError:
+        print("Index out of range")
+    return i
