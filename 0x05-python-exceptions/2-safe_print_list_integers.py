b09eb779c6c350beccd1c8231a20731bbc381e9c
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-04 13:50:32 +0100

    Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    r_counter = 0
+    for counter in range(x):
+        try:
+            print("{:d}".format(my_list[counter]), end="")
+        except (TypeError, ValueError):
+            continue
+        else:
+            r_counter += 1
+    print()
+    return counter
