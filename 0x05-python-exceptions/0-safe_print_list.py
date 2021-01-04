f11d5c988b0ed9dc6f530aec318cd7c5dc30a568
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-04 13:13:26 +0100

    Task 0: fix for x = 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        i = 0
         if x > 0:
-            i = 0
             for elm in my_list:
                 print("{}".format(elm), end="")
                 i += 1
