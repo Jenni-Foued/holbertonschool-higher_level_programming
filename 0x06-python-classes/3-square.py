6b0d5291b8f5672b89bd2549ca405f303f0aa469
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-05 20:34:00 +0100

    Task 4 + fix pep8 for Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,6 +15,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         """Calculates the square's area
         Returns:
