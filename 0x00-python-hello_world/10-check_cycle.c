ce65bd8ce37250adea76c9cb52a2d55058bbd336
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-20 01:00:02 +0100

    Task 10: New approach

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,50 +1,23 @@
 #include "lists.h"
-/**
- * check_element - check if an adress of an element of a singly
- * linked list is redundant.
- *
- * @elm: adress of the element to be checked.
- * @list: pointer to the head of the list.
- *
- * Return: 0 or 1
- */
-
-int check_element(listint_t *elm, listint_t *list)
-{
-	int flag = 0;
-
-	while (list)
-	{
-		if (elm == list)
-			flag++;
-
-		if (flag > 1)
-			return (1);
-		list = list->next;
-	}
-	return (0);
-}
 
 /**
  * check_cycle - checks if a list has a cycle.
+ *
  * @list: pointer to head of list.
+ *
  * Return: 0 if there is no cycle, 1 if there is a cycle.
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *temp_list;
-	int flag = 0;
+	listint_t *first_pointer = list, *second_pointer = list;
 
-	temp_list = list;
-	if (temp_list == NULL || temp_list->next == NULL)
-		return (0);
-	while (temp_list)
+	while (first_pointer && second_pointer && second_pointer->next)
 	{
-		flag = check_element(temp_list, list);
-		if (flag)
+		first_pointer = first_pointer->next;
+		second_pointer = second_pointer->next->next;
+		if (first_pointer == second_pointer)
 			return (1);
-		temp_list = temp_list->next;
 	}
 	return (0);
 }
