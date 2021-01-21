f4679626b8e052bcdfb271cc5c5e03f95f0f5f3d
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-22 00:06:24 +0100

    Task 0: fixed for case a is None

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -19,9 +19,9 @@ def add_integer(a, b=98):
         Returns:
             (int) : The sum of a and b
         """
-    if type(a) not in [int, float]:
+    if a is None or type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if b is None or type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
