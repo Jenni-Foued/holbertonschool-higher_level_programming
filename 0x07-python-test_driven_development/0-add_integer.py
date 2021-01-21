e1ebc169bdec8d01b2057056ea811dbe500b88da
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-21 23:48:38 +0100

    Task 0: fixing float conversation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -22,8 +22,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        int(a)
-    if type(b) is float:
-        int(b)
-    return a + b
+    return int(a) + int(b)
