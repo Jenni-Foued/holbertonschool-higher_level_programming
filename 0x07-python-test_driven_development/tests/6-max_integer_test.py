004e5eb65f53a6cfe0f9eda751771f1c29ffbbeb
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-23 02:30:39 +0100

    Task 6: basic tests

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    unittest class for max_integer
+    """
+    def test_module_doc(self):
+        """Test module docstring"""
+        m = __import__('6-max_integer').__doc__
+        self.assertTrue(len(m) > 1)
+    
+    def test_function_doc(self):
+        """Test function docstring"""
+        f = max_integer.__doc__
+        self.assertTrue(len(f) > 1)
+
+if __name__ == "__main__":
+    unittest.main()
