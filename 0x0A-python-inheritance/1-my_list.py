d10b7aeb9f6e029dac7e65eaf7b09d922379067c
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-24 22:18:13 +0100

    Task 1: removed the int method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,9 +6,6 @@ Defines  Mylist class
 
 class MyList(list):
     """list subclass"""
-    def __init__(self):
-        """initializes the list"""
-        super().__init__()
 
     def print_sorted(self):
         """prints the list sorted (ascending sort)"""
