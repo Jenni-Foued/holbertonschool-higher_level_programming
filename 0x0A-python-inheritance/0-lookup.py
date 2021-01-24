735b9f266d4e4c6a18ea54c259f6b31aaffdf850
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-24 21:37:39 +0100

    Task 0: easy way

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,4 @@
 
 def lookup(obj):
     ''' returns the list of available attributes and methods of an object'''
-    object_methods = [method_name for method_name in dir(obj)
-                      if callable(getattr(obj, method_name))]
-    return object_methods
+    return dir(obj)
