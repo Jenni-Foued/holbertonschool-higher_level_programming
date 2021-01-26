e61b57545d46f26d4863d5f8dd2818df856ce4cc
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-26 12:26:57 +0100

    Task 0: no blank line after print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ Contains read_file() function
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="UTF8") as f:
-        print(f.read())
+        print(f.read(), end="")
