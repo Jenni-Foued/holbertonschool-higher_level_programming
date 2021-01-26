b7af891337c7c0d2e2f683223b69a4798b1619d4
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-26 12:21:34 +0100

    Task 0: added encoding option to open function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ Contains read_file() function
 
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding="UTF8") as f:
         print(f.read())
