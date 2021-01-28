3fab83c777ae7ca7c6809d179578b30f6d233047
Jenni-Foued <medfouedjenni@gmail.com>
2021-01-28 02:51:37 +0100

    base: to_json_string: correct return for empty or None case

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,5 +29,5 @@ class Base:
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
         if json_string is None or len(json_string) == 0:
-            json_string = []
+            return "[]"
         return json.loads(json_string)
