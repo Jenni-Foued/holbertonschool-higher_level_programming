e62868f021ee6b02fcbc7d694da3904ba2d71d29
Jenni-Foued <medfouedjenni@gmail.com>
2021-03-02 21:50:12 +0100

    TASK 7: fix for FK creation

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,7 @@
 -- This script creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
 CREATE DATABASE  IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
-state_id INT NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
-name VARCHAR(256) NOT NULL);
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+);
