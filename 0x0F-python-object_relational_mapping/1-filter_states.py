c9e707fc21b5b9f52982c5450ab259d7a380a233
Jenni-Foued <medfouedjenni@gmail.com>
2021-03-26 17:28:14 +0100

    Tasks from 0 to 6 & README.md

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A python script that lists all states with a name starting with N
+from the database hbtn_0e_0_usa
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+	conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+						   passwd=argv[2], db=argv[3], charset="utf8")
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+	query_rows = cur.fetchall()
+	for row in query_rows:
+		print(row)
+	cur.close()
+	conn.close()
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+	__tablename__ = 'states'
+	id = Column(Integer, primary_key=True)
+	name =  Column(String(128), nullable=False)
