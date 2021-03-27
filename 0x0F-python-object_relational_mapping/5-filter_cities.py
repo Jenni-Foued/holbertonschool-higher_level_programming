51f578b883e32a0b702634753b9fe79f6df30533
Jenni-Foued <medfouedjenni@gmail.com>
2021-03-27 16:31:21 +0100

    Fix: pep8 indention

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,13 +9,13 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-	conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-						   passwd=argv[2], db=argv[3], charset="utf8")
-	cur = conn.cursor()
-	cur.execute("SELECT cities.name FROM cities \
-				JOIN states ON cities.state_id = states.id \
-				WHERE states.name LIKE %s ORDER BY cities.id ASC", (argv[4],))
-	query_rows = cur.fetchall()
-	print(", ".join(city[0] for city in query_rows))
-	cur.close()
-	conn.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM cities \
+                JOIN states ON cities.state_id = states.id \
+                WHERE states.name LIKE %s ORDER BY cities.id ASC", (argv[4],))
+    query_rows = cur.fetchall()
+    print(", ".join(city[0] for city in query_rows))
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-
 """
-
+Python file that contains the class definition of a State and an instance
+Base = declarative_base()
 """
 
 from sqlalchemy import Column, Integer, String
@@ -11,6 +11,9 @@ Base = declarative_base()
 
 
 class State(Base):
-	__tablename__ = 'states'
-	id = Column(Integer, primary_key=True)
-	name =  Column(String(128), nullable=False)
+    """
+    State class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
