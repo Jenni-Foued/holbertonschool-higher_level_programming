56e621110d1b3eff6dc2034f50ae8f21ef9fc324
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-08 16:28:47 +0100

    silent mode

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in an URL as an argument, sends a GET request to the URL and displays the body of the response
-curl "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
+curl "$1" -sX GET -H "X-HolbertonSchool-User-Id: 98"
