4356318862c1e489cdadac4fca9d50a175d25de0
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-08 16:21:14 +0100

    -I option

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in an URL and displays all HTTP methods the server will accept
-curl -sX OPTIONS "$1" | grep "Allow:" | cut -c8-
+curl -sIX OPTIONS "$1" | grep "Allow:" | cut -c8-
