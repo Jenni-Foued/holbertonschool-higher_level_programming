f2adb2099f63dda5f4fa5e65de8f65b0b5f52fe0
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-08 15:44:17 +0100

    Task 0, 1, 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in an URL, send a request to that URL
-# and displays the size of the body of the response
+# Bash script that takes in an URL, send a request to that URL and displays the size of the body of the response
 curl -sI "$1" | grep "Content-Length" | cut -d" " -f2
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Bash script that takes in an URL, sends a GET request to the URL
-# and displays the body of the response
-
+# Bash script that takes in an URL, sends a GET request to the URL and displays the body of the response
 curl -s "$1"
