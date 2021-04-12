832b6ec33895a83da92f9b5f650a2402cf962174
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-12 16:20:35 +0100

    Task 10: 16 min :o

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Takes in a github user name and a repository name
+and list the recent 10 commits
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    r = requests.get("https://api.github.com/repos/{}/{}/commits"
+                     .format(argv[2], argv[1]))
+    r_dict = r.json()
+    for commit in r_dict[:10]:
+        print("{}: {}".format(commit.get('sha'), commit.get('author')))
