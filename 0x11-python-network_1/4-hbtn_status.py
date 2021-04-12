5b88c35cda553db10b839775178a690671b1cf69
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-12 13:57:43 +0100

    Task 4: fetch URL using requests module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status
+using the module requests
+"""
+if __name__ == "__main__":
+    import requests
+    r = requests.get('https://intranet.hbtn.io/status')
+    html = r.text
+    print('Body response')
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
