5b19f218e14d1d6551679affa3388ebe5747b75b
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-12 16:30:07 +0100

    last try

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     r = requests.get("https://api.github.com/repos/{}/{}/commits"
                      .format(argv[2], argv[1]))
     r_dict = r.json()
-    try:
-        for commit in r_dict[:10]:
-            print("{}: {}".format(commit.get('sha'), commit.get('author')))
-    except Exception:
-        pass
+    for commit in r_dict[:10]:
+        print("{}: {}"
+              .format(commit.get('sha'),
+                      commit.get('commit').get('author').get('name')))
