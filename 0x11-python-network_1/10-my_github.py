2b9751e265116dbf7c7b154e9dbccf450e33aae8
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-12 15:52:21 +0100

    checker

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,4 +7,7 @@ if __name__ == '__main__':
     import requests
     from sys import argv
     r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
-    print(r.json()['id'])
+    try:
+        print(r.json()['id'])
+    except Exception:
+        print('None')
