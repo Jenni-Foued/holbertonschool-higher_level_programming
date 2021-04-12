8880dd2a8bae49e9723b021ad5206d8f5926a6aa
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-12 14:05:00 +0100

    Task 5:display request id using requests module

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL
+and displays the value of the variable X-Request-Id in the response header
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.get(sys.argv[1])
+    req_id = r.headers['X-Request-Id']
+    print(req_id)
