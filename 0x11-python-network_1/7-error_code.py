eb86c7f03e5e28a2eb91b949549548c6d785697e
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-12 15:17:10 +0100

    Task 7: removed convertion to int

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     import requests
     from sys import argv
     r = requests.get(argv[1])
-    code = int(r.status_code)
-    if code >= 400:
-        print ("Error code: {}".format(code))
+    if r.status_code >= 400:
+        print ("Error code: {}".format(r.status_code))
     else:
         print(r.text)
