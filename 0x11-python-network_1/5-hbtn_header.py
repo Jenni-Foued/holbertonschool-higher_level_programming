c9a83a9e36e7ecab4be2198d1a4b54701d74cee3
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-12 14:12:59 +0100

    quick fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL
-and displays the value of the variable X-Request-Id in the response header
+takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
 """
 if __name__ == "__main__":
     import requests
-    import sys
-    r = requests.get(sys.argv[1])
-    req_id = r.headers['X-Request-Id']
-    print(req_id)
+    from sys import argv
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
